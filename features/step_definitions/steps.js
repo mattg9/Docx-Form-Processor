617df742aa82abf9bfb89e237dc17e365cc248c5
Matthew Gaston <matthew.gaston@broadcom.com>
2024-05-07 10:18:43 -0400

    documentation

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -13,6 +13,11 @@ let jsonData;
 let templateForm;
 const estateForm = "rcp-e-74b-0921-result.docx";
 
+/**
+ * Define the jsonData from parsed file given
+ * @param {string} jsonFileName - The name of the JSON file
+ * @throws {Error} - If the JSON file does not exist
+ */
 Given('a JSON file named {string}', async function (jsonFileName) {
     const jsonFilePath = `${DATA_DIR}/${jsonFileName}`;
     // Check if the file exists
@@ -22,6 +27,11 @@ Given('a JSON file named {string}', async function (jsonFileName) {
     jsonData = JSON.parse(fs.readFileSync(jsonFilePath));
 });
 
+/**
+ * Define the templateForm from given docx file
+ * @param {string} docxFileName - The name of the template file
+ * @throws {Error} - If the template file does not exist or is not a .docx file
+ */
 Given('a template file named {string}', async function (docxFileName) {
     const docxFilePath = `${TEMPLATE_DIR}/${docxFileName}`;
     // Check if the file exists
@@ -39,6 +49,12 @@ When('I create file from template using values from the JSON', async function ()
     writeDocument();
 });
 
+/**
+ * Check given file for expected content
+ * @param {string} file - The name of the file
+ * @param {DataTable} table - Table containing expectations
+ * @throws {Error} - If expectations are not met
+ */
 Then('file {string} is expected:', async function (file, table) {
     content = await readDocument(`${RESULT_DIR}/${file}`);
 
@@ -59,18 +75,36 @@ Then('file {string} is expected:', async function (file, table) {
     }
 });
 
+/**
+ * Check given file does not contain text provided:
+ * @param {string} file - The name of the file
+ * @param {string} lines - Lines that should not be present in the file
+ * @throws {AssertionError} - If the lines are found in the file
+ */
 Then('file {string} should not contain the following lines:', async function (file, lines) {
     const lineToCheck = lines.split('\n').map(line => line.trim()).join(' ');
     const fileContent = await readDocument(`${RESULT_DIR}/${file}`);
     expect(fileContent).to.not.contain(lineToCheck);
   });
 
+/**
+ * Check given file does contain text provided:
+ * @param {string} file - The name of the file
+ * @param {string} lines - Lines that should not be present in the file
+ * @throws {AssertionError} - If the lines are not found in the file
+ */
 Then('file {string} should contain the following lines:', async function (file, lines) {
     const lineToCheck = lines.split('\n').map(line => line.trim()).join(' ');
     const fileContent = await readDocument(`${RESULT_DIR}/${file}`);
     expect(fileContent).to.contain(lineToCheck);
 });
 
+/**
+ * Check file contains the following table entires, name and reason, separated by a tab
+ * @param {string} file - The name of the file
+ * @param {DataTable} table - Table containing expected names and corresponding reasons
+ * @throws {Error} - If expected content is not found in the file
+ */
 Then('file {string} should contain the following table:', async function (file, table) {
     content = await readDocument(`${RESULT_DIR}/${file}`);
 
